95d7d85e70433076191e69585091f14e272b6bc9
Ismael <ismaelfelixfavela@gmail.com>
2023-10-25 00:02:20 -0700

    Se obtienen medicos, se eliminan opciones innecesarias para medico y se renombra json es

--- src/app/admin/doctors/alldoctors/doctors.service.ts
+++ src/app/admin/doctors/alldoctors/doctors.service.ts
@@ -3,34 +3,37 @@ import { BehaviorSubject } from 'rxjs';
 import { Doctors } from './doctors.model';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { UnsubscribeOnDestroyAdapter } from '@shared';
+import { Medico, Medicos } from 'app/interfaces/Medico.interface';
 @Injectable()
 export class DoctorsService extends UnsubscribeOnDestroyAdapter {
   private readonly API_URL = 'assets/data/doctors.json';
   isTblLoading = true;
-  dataChange: BehaviorSubject<Doctors[]> = new BehaviorSubject<Doctors[]>([]);
+  dataChange: BehaviorSubject<Medico[]> = new BehaviorSubject<Medico[]>([]);
   // Temporarily stores data from dialogs
   dialogData!: Doctors;
   constructor(private httpClient: HttpClient) {
     super();
   }
-  get data(): Doctors[] {
+  get data(): Medico[] {
     return this.dataChange.value;
   }
   getDialogData() {
     return this.dialogData;
   }
   /** CRUD METHODS */
-  getAllDoctorss(): void {
-    this.subs.sink = this.httpClient.get<Doctors[]>(this.API_URL).subscribe({
-      next: (data) => {
-        this.isTblLoading = false;
-        this.dataChange.next(data);
-      },
-      error: (error: HttpErrorResponse) => {
-        this.isTblLoading = false;
-        console.log(error.name + ' ' + error.message);
-      },
-    });
+  getAllDoctorss(){
+
+    return this.httpClient.get<Medicos>(`http://localhost:8000/api/medicos`);
+    // this.subs.sink = this.httpClient.get<Medicos>(`http://localhost:8000/api/medicos`).subscribe({
+    //   next: (data) => {
+    //     this.isTblLoading = false;
+    //     this.dataChange.next(data.medicos);
+    //   },
+    //   error: (error: HttpErrorResponse) => {
+    //     this.isTblLoading = false;
+    //     console.log(error.name + ' ' + error.message);
+    //   },
+    // });
   }
   addDoctors(doctors: Doctors): void {
     this.dialogData = doctors;
---
+++ src/app/interfaces/Medico.interface.ts
@@ -0,0 +1,35 @@
+export interface Medicos {
+    msg:     string;
+    medicos: Medico[];
+}
+
+export interface Medico {
+    id_medico:           number;
+    nombre:              string;
+    apellidos:           string;
+    correo:              string;
+    especialidad:        string;
+    cedula:              string;
+    permiso_secre_salud: string;
+    id_edificio:         number;
+    id_piso:             number;
+    google:              boolean;
+    rol:                 string;
+    telefono:            string;
+    fecha_registro:      Date;
+    createdAt:           Date;
+    updatedAt:           Date;
+}
+
+export interface MedicoTable {
+    idMedico:           number;
+    nombre:              string;
+    apellidos:           string;
+    correo:              string;
+    especialidad:        string;
+    cedula:              string;
+    permisoSecreSalud: string;
+    idEdificio:         number;
+    idPiso:             number;
+    telefono:           string;
+}
